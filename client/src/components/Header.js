cebe3c2c4b659615256373f580599d2c1b627a10
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-16 16:09:33 -0400

    Creating and importing page header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div>
+			<img 
+				src="https://assets-global.website-files.com/60d188ae990678453cff815f/60d1932a2a24f32cd519c9bb_RR%20Logo%2BWordmark.svg" 
+				loading="lazy" 
+				alt=""
+				className="mt-3 ml-2"
+			/>
+			<h1 
+				className="text-center">
+					driver
+						<span className="text-primary">hub</span>
+			</h1>
+		</div>
+	)
+};
+
+export default Header;
