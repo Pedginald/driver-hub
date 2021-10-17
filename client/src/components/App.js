6cc440f5bc4154fecadd02e6aeac0a7768c178bc
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-17 19:12:53 -0400

    Importing MainHub to App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import MainHub from './MainHub';
 
 const App = () => {
 	return (
 		<div>
 			<Header />
+			<MainHub />
 		</div>
 	)
 };
