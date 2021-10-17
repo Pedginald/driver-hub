6a463c31f23f3faad64a805d8a6e9aa7e8232a63
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-17 19:12:27 -0400

    Replacing div with more semantic header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,19 +2,18 @@ import React from 'react';
 
 const Header = () => {
 	return (
-		<div>
+		<header>
 			<img 
 				src="https://assets-global.website-files.com/60d188ae990678453cff815f/60d1932a2a24f32cd519c9bb_RR%20Logo%2BWordmark.svg" 
 				loading="lazy" 
 				alt=""
 				className="mt-3 ml-2"
 			/>
-			<h1 
-				className="text-center">
-					driver
-						<span className="text-primary">hub</span>
+			<h1 className="text-center">
+				driver
+				<span className="text-primary">hub</span>
 			</h1>
-		</div>
+		</header>
 	)
 };
 
