72895e6ba9bd5c01e96af67a2afe296424522ee2
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-17 19:11:43 -0400

    Creating Drivers component

---
+++ client/src/components/Drivers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Drivers = () => {
+	return (
+		<div className="border col">
+			<h2 className="text-center">Drivers</h2>
+		</div>
+	)
+};
+
+export default Drivers;
