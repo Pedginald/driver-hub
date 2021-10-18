8cd65ba0d38eceaddeddf90de122db0b96ad55dd
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-18 17:02:16 -0400

    Creating file for temporary hardcoded data

---
+++ client/src/data/tempData.js
@@ -0,0 +1,12 @@
+export const orders = [
+	{ id: 'ord1', description: 'Toronto to Barrie', revenue: '100.00', cost: '55.50' },
+	{ id: 'ord2', description: 'Wawa to Sudbury', revenue: '1300.00', cost: '70.00' },
+	{ id: 'ord3', description: 'Chicago to Hamilton', revenue: '1500.00', cost: '95.00' },
+	{ id: 'ord4', description: 'Milton to Oakville', revenue: '60.00', cost: '100.50' }
+];
+
+export const drivers = [
+	{ id: 'drv1', name: 'Jane Doe', orders: [] },
+	{ id: 'drv2', name: 'Harry Potter', orders: [] },
+	{ id: 'drv3', name: 'Trucker Dave', orders: [] }
+];
