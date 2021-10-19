32ab25670f6dfb15caefe3c364f196eb1914f56a
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-19 14:23:14 -0400

    Creating Drivers component and driver card

---
+++ client/src/cards/driver.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const driver = () => {
+	return (
+		<>
+			
+		</>
+	)
+};
+
+export default driver;
--- client/src/components/Drivers.js
+++ client/src/components/Drivers.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { driverDetails } from '../data/tempData';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
 
 const Drivers = () => {
+	const [drivers, setDrivers] = useState(driverDetails);
+
 	return (
-		<div className='col'>
+		<div className='col border'>
 			<h2 className='text-center'>Drivers</h2>
+			{Object.entries(drivers).map(([id, driver]) => {
+				return (
+					<Droppable droppableId={id} key={id}>
+						{(provided, snapshot) => {
+							return (
+								<div
+									ref={provided.innerRef}
+									{...provided.droppableProps}
+								>
+									{driver.name}
+									{driver.orders.map((order, index) => {
+										return (
+											<Draggable draggableId={order.orderId} key={order.orderId} index={index}>
+												{(provided, snapshot) => {
+													return (
+														<div
+															ref={provided.innerRef}
+															{...provided.draggableProps}
+															{...provided.dragHandleProps}
+														>
+															<p>{order.description}</p>
+														</div>
+													);
+												}}
+											</Draggable>
+										);
+									})}
+									{provided.placeholder}
+								</div>
+							);
+						}}
+					</Droppable>
+				);
+			})}
 		</div>
-	)
+	);
 };
 
 export default Drivers;
