75ffea66a43aea4ddb6133c30f960706d33b8759
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-19 18:15:11 -0400

    Exporting draggable orders to their own component

--- client/src/components/UnassignedOrders.js
+++ client/src/components/UnassignedOrders.js
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
 import { orders } from '../data/tempData';
-import { Draggable, Droppable } from 'react-beautiful-dnd';
-import Order from '../cards/Order';
-
-const onDragEnd = () => {
-	
-}
+import { Droppable } from 'react-beautiful-dnd';
+import OrderCard from '../cards/OrderCard';
 
 const UnassignedOrders = ({ id }) => {
 	const [unassignedOrders, setUnassignedOrders] = useState(orders);
@@ -13,7 +9,7 @@ const UnassignedOrders = ({ id }) => {
 	return (
 		<div className='col'>
 			<h2>Unassigned orders</h2>
-			<Droppable droppableId={id} key={id}>
+			<Droppable droppableId={id}>
 				{(provided, snapshot) => {
 					return (
 						<div
@@ -22,19 +18,7 @@ const UnassignedOrders = ({ id }) => {
 						>
 							{unassignedOrders.map((order, index) => {
 								return (
-									<Draggable key={order.orderId} draggableId={order.orderId} index={index}>
-										{(provided, snapshot) => {
-											return (
-												<div className='border'
-													{...provided.draggableProps}
-													{...provided.dragHandleProps}
-													ref={provided.innerRef}
-												>
-													<p>{order.description}</p>
-												</div>
-											);
-										}}
-									</Draggable>
+									<OrderCard key={order.orderId} orderDetails={order} index={index}/>
 								);
 							})}
 							{provided.placeholder}
