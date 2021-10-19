b4ab14b0350abd5da181825739c1005259d3af5e
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-19 14:24:05 -0400

    Implementing state and adding droppable key

--- client/src/components/UnassignedOrders.js
+++ client/src/components/UnassignedOrders.js
@@ -1,36 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { orders } from '../data/tempData';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
+import Order from '../cards/Order';
 
 const onDragEnd = () => {
 	
 }
 
 const UnassignedOrders = ({ id }) => {
+	const [unassignedOrders, setUnassignedOrders] = useState(orders);
+
 	return (
 		<div className='col'>
 			<h2>Unassigned orders</h2>
-			<Droppable droppableId={id}>
+			<Droppable droppableId={id} key={id}>
 				{(provided, snapshot) => {
 					return (
 						<div
 							{...provided.droppableProps}
 							ref={provided.innerRef}
 						>
-							{orders.map((order, index) => {
+							{unassignedOrders.map((order, index) => {
 								return (
-									<Draggable key={order.id} draggableId={order.id} index={index}>
-									{(provided, snapshot) => {
-										return (
-											<div className='border'
-												{...provided.draggableProps}
-												{...provided.dragHandleProps}
-												ref={provided.innerRef}
-											>
-												<p>{order.description}</p>
-											</div>
-										);
-									}}
+									<Draggable key={order.orderId} draggableId={order.orderId} index={index}>
+										{(provided, snapshot) => {
+											return (
+												<div className='border'
+													{...provided.draggableProps}
+													{...provided.dragHandleProps}
+													ref={provided.innerRef}
+												>
+													<p>{order.description}</p>
+												</div>
+											);
+										}}
 									</Draggable>
 								);
 							})}
