8768905da4eda4607802a88c10c96e8ae48fde66
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-19 18:12:04 -0400

    Importing DriverCard to Drivers

--- client/src/components/Drivers.js
+++ client/src/components/Drivers.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { driverDetails } from '../data/tempData';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
+import DriverCard from '../cards/DriverCard';
 
 const Drivers = () => {
 	const [drivers, setDrivers] = useState(driverDetails);
@@ -9,6 +10,7 @@ const Drivers = () => {
 		<div className='col border'>
 			<h2 className='text-center'>Drivers</h2>
 			{Object.entries(drivers).map(([id, driver]) => {
+				console.log(id, driver);
 				return (
 					<Droppable droppableId={id} key={id}>
 						{(provided, snapshot) => {
