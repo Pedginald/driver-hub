8436998b1367478c56857576077e6184707287ad
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-19 13:33:52 -0400

    Changing component ids

--- client/src/components/MainHub.js
+++ client/src/components/MainHub.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import UnassignedOrders from './UnassignedOrders';
 import Drivers from './Drivers';
 import { DragDropContext } from 'react-beautiful-dnd';
+
+const onDragEnd = () => {
+
+}
 
 const MainHub = () => {
 	return (
 		<div className='container-fluid mt-4'>
 			<div className='row'>
-				<DragDropContext onDragEnd={result => console.log(result)}>
-					<UnassignedOrders id='column1'/>
-					<Drivers id='column2'/>
+				<DragDropContext onDragEnd={onDragEnd}>
+					<UnassignedOrders id='unassignedOrders'/>
+					<Drivers id='drivers'/>
 				</DragDropContext>
 			</div>
 		</div>
