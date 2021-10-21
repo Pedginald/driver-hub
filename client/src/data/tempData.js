c54b8d21eb30423374b1774b9f8a159b0d161384
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-21 09:45:39 -0400

    Consolidating initial data to one object/database

--- client/src/data/tempData.js
+++ client/src/data/tempData.js
@@ -1,46 +1,58 @@
-export const orders = [
-	{ 
-		orderId: 'ord1', 
-		location: 'Toronto to Barrie', 
-		description: 'Construction Materials', 
-		revenue: '100.00', 
-		cost: '55.50' 
+const dataBase = {
+	orderTable: {
+		ord1: {
+			orderId: 'ord1', 
+			location: 'Toronto to Barrie', 
+			description: 'Construction Materials', 
+			revenue: '100.00', 
+			cost: '55.50' 
+		},
+		ord2: {
+			orderId: 'ord2', 
+			location: 'Wawa to Sudbury', 
+			description: 'Construction Materials', 
+			revenue: '1300.00', 
+			cost: '70.00'
+		},
+		ord3: { 
+			orderId: 'ord3', 
+			location: 'Chicago to Hamilton', 
+			description: 'Wood and Lumber', 
+			revenue: '1500.00', 
+			cost: '95.00' 
+		},
+		ord4: { 
+			orderId: 'ord4', 
+			location: 'Milton to Oakville', 
+			description: 'Wood and Lumber', 
+			revenue: '60.00', 
+			cost: '100.50' 
+		}
 	},
-	{ 
-		orderId: 'ord4', 
-		location: 'Milton to Oakville', 
-		description: 'Wood and Lumber', 
-		revenue: '60.00', 
-		cost: '100.50' 
-	}
-];
 
-export const driverDB = {
-	drv1: {
-		name: "Steve Williams",
-		orders: [
-			{ 
-				orderId: 'ord2', 
-				location: 'Wawa to Sudbury', 
-				description: 'Construction Materials', 
-				revenue: '1300.00', cost: '70.00' 
-			}
-		]
+	unassignedOrderTable: {
+		unassigned: {
+			orderIds: ['ord1', 'ord2', 'ord3', 'ord4']
+		}
 	},
-	drv2: {
-		name: 'Chris Horton',
-		orders: [
-			{ 
-				orderId: 'ord3', 
-				location: 'Chicago to Hamilton', 
-				description: 'Wood and Lumber', 
-				revenue: '1500.00', 
-				cost: '95.00' 
-			}
-		]
-	},
-	drv3: {
-		name: 'Alex Novak',
-		orders: []
+
+	driverTable: {
+		drv1: {
+			driverId: 'drv1',
+			name: "Steve Williams",
+			orderIds: []
+		},
+		drv2: {
+			driverId: 'drv2',
+			name: 'Chris Horton',
+			orderIds: []
+		},
+		drv3: {
+			driverId: 'drv3',
+			name: 'Alex Novak',
+			orderIds: []
+		}
 	}
 };
+
+export default dataBase;
