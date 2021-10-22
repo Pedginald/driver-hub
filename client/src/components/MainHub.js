47288edd31a44c8d0246429fa3823eb4a4be181f
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-21 20:36:37 -0400

    Writing handleDragEnd function

--- client/src/components/MainHub.js
+++ client/src/components/MainHub.js
@@ -3,17 +3,43 @@ import UnassignedOrders from './UnassignedOrders';
 import Drivers from './Drivers';
 import { DragDropContext } from 'react-beautiful-dnd';
 
-const onDragEnd = () => {
+const MainHub = ({ state, setState }) => {
+	const { driverTable, orderTable, unassignedOrderTable } = state;
+	
+	const handleDragEnd = result => {
+		const { destination, source, draggableId } = result;
 
-}
+		if (!destination) return;
+	
+		if (
+			 destination.droppableId === source.droppableId &&
+			 destination.index === source.index
+		) return;
 
-const MainHub = () => {
+		const dropArea = state[source.droppableId];
+		const newOrderIds = Array.from(dropArea.orderIds);
+		newOrderIds.splice(source.index, 1);
+		newOrderIds.splice(destination.index, 0, draggableId);
+		
+		const newDropArea = {
+			...dropArea,
+			orderIds : newOrderIds
+		};
+
+	};
+
 	return (
 		<div className='container-fluid mt-4'>
 			<div className='row'>
-				<DragDropContext onDragEnd={onDragEnd}>
-					<UnassignedOrders columnId='unassignedOrders'/>
-					<Drivers columnId='drivers'/>
+				<DragDropContext onDragEnd={(result) => handleDragEnd(result)}>
+					<UnassignedOrders 
+						contents={unassignedOrderTable}
+						allOrders={orderTable}
+					/>
+					<Drivers 
+						drivers={driverTable}
+						allOrders={orderTable}
+					/>
 				</DragDropContext>
 			</div>
 		</div>
