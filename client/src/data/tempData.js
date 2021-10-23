2e0071d5cb35c2258595b81c1b9319d6e829e12d
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-23 13:37:36 -0400

    Tyding up

--- client/src/data/tempData.js
+++ client/src/data/tempData.js
@@ -5,42 +5,42 @@ const dataBase = {
 			location: 'Toronto to Barrie', 
 			description: 'Construction Materials', 
 			revenue: 4200.00, 
-			cost: 100.00
+			cost: 100.00,
 		},
 		ord2: {
 			orderId: 'ord2', 
 			location: 'Wawa to Sudbury', 
 			description: 'Construction Materials', 
 			revenue: 3948.45, 
-			cost: 71.38
+			cost: 71.38,
 		},
 		ord3: { 
 			orderId: 'ord3', 
 			location: 'Chicago to Hamilton', 
 			description: 'Wood and Lumber', 
 			revenue: 1500.00, 
-			cost: 95.00 
+			cost: 95.00,
 		},
 		ord4: { 
 			orderId: 'ord4', 
 			location: 'Milton to Oakville', 
 			description: 'Wood and Lumber', 
 			revenue: 60.00, 
-			cost: 100.50 
+			cost: 100.50,
 		},
 		ord5: { 
 			orderId: 'ord5', 
 			location: 'Toronto to New York', 
 			description: 'Meat', 
 			revenue: 6739.72, 
-			cost: 279.17
+			cost: 279.17,
 		},
 		ord6: { 
 			orderId: 'ord6', 
 			location: 'London to Manchester', 
 			description: 'Meat', 
 			revenue: 3618.08, 
-			cost: 537.91
+			cost: 537.91,
 		},
 	},
 
