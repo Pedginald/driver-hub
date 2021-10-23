1503a97cdd7532c9b0f071b204ef2d49b23953dd
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-23 13:36:28 -0400

    Moving state to mainhub

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './Header';
 import MainHub from './MainHub';
 import dataBase from '../data/tempData';
 
 const App = () => {
-	const [initialData, setInitialData] = useState(dataBase);
-
+	
 	return (
 		<>
 			<Header />
-			<MainHub state={initialData} setState={setInitialData}/>
+			<MainHub allData={dataBase}/>
 		</>
 	)
 };
