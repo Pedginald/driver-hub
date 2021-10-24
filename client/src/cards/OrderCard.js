ae7023eecd95ceff9880635e7cc95637bfec2c3c
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-23 20:56:38 -0400

    Setting states and conditional rendering

--- client/src/cards/OrderCard.js
+++ client/src/cards/OrderCard.js
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
+import { GiSave } from 'react-icons/gi';
+import { ImPencil2 } from 'react-icons/im'
 
-const OrderCard = ({ orderDetails, index }) => {
+const OrderCard = ({ orderDetails, index, status }) => {
 	const { orderId, description, revenue, cost } = orderDetails;
-	const [assigned, setAssigned] = useState(false);
+	const [isAssigned] = useState(status);
+	const [isEditing, setIsEditing] = useState(false);
+
+	const handleChange = () => {
+		
+	}
+
+	const handleClick = (orderId) => {
+		setIsEditing(!isEditing);
+	}
+
+	console.log(isEditing);
 
 	return (
 		<Draggable 
@@ -17,10 +30,31 @@ const OrderCard = ({ orderDetails, index }) => {
 						{...provided.draggableProps}
 						{...provided.dragHandleProps}
 						ref={provided.innerRef}
-					>
+					>	
 						<p>{description}</p>
-						<p>{revenue}</p>
-						<p>{cost}</p>
+						{isEditing ?
+							<>
+								<input onChange={handleChange}></input>
+								<input onChange={handleChange}></input>
+								{isAssigned ?
+									undefined :
+									<button onClick={() => handleClick(orderId)}>
+										<GiSave />
+									</button>
+								}
+							</>
+							:
+							<>
+								<p>{revenue}</p>
+								<p>{cost}</p>
+								{isAssigned ?
+									undefined :
+									<button onClick={() => handleClick(orderId)}>
+										<ImPencil2 />
+									</button>
+								}
+							</>
+						}
 					</div>
 				);
 			}}
